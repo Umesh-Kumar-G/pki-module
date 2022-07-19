48f697f00bb3091435914d8d2794b23495345565
Sudan <sudan.shrestha@f1soft.com>
2022-07-19 11:40:17 +0545

    init

---
+++ src/test/java/com/fonepay/pkimodule/PkiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fonepay.pkimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkiModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
